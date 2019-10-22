11a718b6dc25fb0e2f8690a6186f46087149d70e
Juan C <juan.cisilino@gmail.com>
2019-10-22 13:08:09 -0300

    .

--- AppMuvi/app/src/main/java/com/example/appmuvi/view/MainActivity.java
+++ AppMuvi/app/src/main/java/com/example/appmuvi/view/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements FragmentListaDePe
 
 
         pegarFragment(new FragmentListaDePeliculas());
-        pegarFragmentFamoso(new FragmentListaFamosos());
+       // pegarFragmentFamoso(new FragmentListaFamosos());
     }
 
 
